736773bbdf16862dd5e0a5aeb3e969ad828dfb3d
hydrofalll <djarastafarabg@gmail.com>
2020-01-30 15:09:09 +0400

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%!d+7(1uvvk@&r5%5qk6321c)8l!fx+r04ei3ib=09ac)%t3m-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['hydrofalll.ddns.net','127.0.0.1']
 
 
 # Application definition
